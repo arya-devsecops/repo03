82fe16639b22cf43faaef37a3c516cfa66997829
Arya Kumar Arya <144605735+Arya5596@users.noreply.github.com>
2023-11-21 18:43:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "exp" {
   name     = var.resource_name
   location = var.location
 }
 
 resource "azurerm_storage_account" "test" {
   name                     = var.storage_name
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+  resource_group_name      = azurerm_resource_group.exp.name
+  location                 = azurerm_resource_group.exp.location
   account_tier             = var.account_tier
   account_replication_type = var.account_replication_type
 
